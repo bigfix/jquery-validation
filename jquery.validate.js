9aa53f9241224ba349f9fe1b2c801be0f431b63b
John Firebaugh <john_firebaugh@us.ibm.com>
2011-02-01 14:46:10 -0800

    jQuery 1.5 compatibility

--- jquery.validate.js
+++ jquery.validate.js
@@ -1086,20 +1086,32 @@ $.format = $.validator.format;
 // usage: $.ajax({ mode: "abort"[, port: "uniqueport"]});
 // if mode:"abort" is used, the previous request on that port (port can be undefined) is aborted via XMLHttpRequest.abort() 
 ;(function($) {
-	var ajax = $.ajax;
-	var pendingRequests = {};
-	$.ajax = function(settings) {
-		// create settings for compatibility with ajaxSetup
-		settings = $.extend(settings, $.extend({}, $.ajaxSettings, settings));
-		var port = settings.port;
-		if (settings.mode == "abort") {
-			if ( pendingRequests[port] ) {
-				pendingRequests[port].abort();
-			}
-			return (pendingRequests[port] = ajax.apply(this, arguments));
-		}
-		return ajax.apply(this, arguments);
-	};
+  var pendingRequests = {};
+  if ($.ajaxPrefilter) {
+    $.ajaxPrefilter(function (settings, original, jqXHR) {
+      var port = settings.port;
+      if (settings.mode == "abort") {
+        if ( pendingRequests[port] ) {
+          pendingRequests[port].abort();
+        }
+        pendingRequests[port] = jqXHR;
+      }
+    });
+  } else {
+    var ajax = $.ajax;
+    $.ajax = function(settings) {
+      // create settings for compatibility with ajaxSetup
+      settings = $.extend(settings, $.extend({}, $.ajaxSettings, settings));
+      var port = settings.port;
+      if (settings.mode == "abort") {
+        if ( pendingRequests[port] ) {
+          pendingRequests[port].abort();
+        }
+        return (pendingRequests[port] = ajax.apply(this, arguments));
+      }
+      return ajax.apply(this, arguments);
+    };
+  }
 })(jQuery);
 
 // provides cross-browser focusin and focusout events
